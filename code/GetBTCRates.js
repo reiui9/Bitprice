e91c5bf734df5c5c639f478d1812b182382687bb
Yubi Lee <eubnara@naver.com>
2019-07-07 14:57:15 +0900

    It malfunctions with `const`, `let` keywords.
    https://stackoverflow.com/questions/56919702/does-bixby-really-support-es6-const-let-keywords-malfunction

--- code/GetBTCRates.js
+++ code/GetBTCRates.js
@@ -1,19 +1,18 @@
 var http = require('http')
 var console = require('console')
 var config = require('config')
-module.exports.function = function findShoe () {
+module.exports.function = function getBTCRates () {
   console.log("GET /ticker without any parameter!)")
   // Read the remote.url value from capsule.properties
   var response = http.getUrl(config.get('blockchain.url') + '/ticker', { format: 'json' });
 
-  const items = [];
-  for (let currency in response) {
-      const item = response[currency];
-      item['quarterBefore'] = item['15m'];
-      // delete item['15m'];
+  var items = [];
+  for (var currency in response) {
+      var item = response[currency];
+      item.quarterBefore = item['15m']
+      delete item['15m'];
       item.currency = currency;
       items.push(item)
   }
-  console.log(items);
   return items;
 }
