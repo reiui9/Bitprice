8386345ee013ee556dc0b13560f14785efee36d1
Youngin Lee <reiui9@naver.com>
2019-07-14 16:00:54 +0900

    Action and Structures

---
+++ code/GetCoins.js
@@ -0,0 +1,7 @@
+// get a coin by using selection
+// if exchange is not defined, set it as 'USD'
+// return 'Coins' model
+
+module.exports.function = function getCoins (arg1, arg2, ...) {
+  return {}
+}
