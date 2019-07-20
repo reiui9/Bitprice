30b6713d84bb99f15cef088a4e35373d58a6055a
Youngin Lee <reiui9@naver.com>
2019-07-20 17:36:08 +0900

    Coin/Exchange Structure 구성

--- code/GetBTCRates.js
+++ code/GetBTCRates.js
@@ -2,7 +2,7 @@ var http = require('http')
 var console = require('console')
 var config = require('config')
 
-module.exports.function = function getBTCRates () {
+module.exports.function = function getBTCRates (coins) {
   console.log("GET /ticker without any parameter!)")
   // Read the remote.url value from capsule.properties
   var response = http.getUrl(config.get('blockchain.url') + '/ticker', { format: 'json' });
@@ -10,18 +10,26 @@ module.exports.function = function getBTCRates () {
   // image setting
   var images=[]; images[0]=config.get('image.bitcoin'); images[1]=config.get('image.ethereum'); images[2]=config.get('image.litecoin');
 
+  var result;
   var items = [];
   for (var currency in response) {
       var item = response[currency];
       // image random setting
       item.image = {}; item.image.url = images[Math.floor(item['15m'] % 3)]
       item.chart = {}; item.chart.url = config.get('chart.bitcoin');
+      item.coins = coins;
       item.quarterBefore = item['15m']
       delete item['15m'];
       item.currency = currency;
       items.push(item)
   }
-  return items;
+  if(coins.coin == 'many'){
+    result = items;
+  } else {
+    items[0].currency = coins.coin
+    result = items[0];
+  }
+  return result;
 }
 
 
--- code/GetCoins.js
+++ code/GetCoins.js
@@ -2,6 +2,13 @@
 // if exchange is not defined, set it as 'USD'
 // return 'Coins' model
 
-module.exports.function = function getCoins (arg1, arg2, ...) {
-  return {}
+module.exports.function = function getCoins (coin, exchange) {
+  var coins = {coin:{},exchange:{}};
+  if(coin == undefined)
+    coin = 'many'
+  if(exchange == undefined)
+    exchange ='usd'
+  coins.coin = coin;
+  coins.exchange = exchange;
+  return coins
 }
