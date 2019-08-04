e312a86ab027c2c656aba7950d716b8e21a14d17
Yubi Lee <eubnara@naver.com>
2019-08-04 11:08:24 +0900

    [#7] change price notation

--- code/GetBTCRates.js
+++ code/GetBTCRates.js
@@ -26,7 +26,7 @@ function getCoinNews (coin, count) {
   // GET 'https://cryptocontrol.io/api/v1/public/news/coin/{slug}?key={apiKey}'
   var response = http.getUrl(apiUrl + '/api/v1/public/news/coin/' + slug[coin] + '?key=' + apiKey, { format: 'json' })
   var results = []
-  for (var i=0; i<count; i++) {
+  for (var i = 0; i < count; i++) {
     var news = response[i]
     var ret = {}
     ret.title = news.title
@@ -35,8 +35,55 @@ function getCoinNews (coin, count) {
     ret.url = news.url
     results.push(ret)
   }
-  console.log(results)
   return results
+}
+
+// https://gist.github.com/jiggzson/b5f489af9ad931e3d186
+function scientificToDecimal(num) {
+  // if the number is in scientific notation remove it
+  if (/\d+\.?\d*e[\+\-]*\d+/i.test(num)) {
+      var zero = '0',
+          parts = String(num).toLowerCase().split('e'), // split into coeff and exponent
+          e = parts.pop(), // store the exponential part
+          l = Math.abs(e), // get the number of zeros
+          sign = e / l,
+          coeff_array = parts[0].split('.')
+      if (sign === -1) {
+          coeff_array[0] = Math.abs(coeff_array[0]);
+          num = '-'+zero + '.' + new Array(l).join(zero) + coeff_array.join('')
+      } else {
+          var dec = coeff_array[1]
+          if(dec) l = l - dec.length
+          num = coeff_array.join('') + new Array(l+1).join(zero)
+      }
+  }
+  
+  return num
+}
+
+function decimalToKRW(decimal) {
+  var 억 = Math.pow(10, 8)
+  var 만 = Math.pow(10, 4)
+  var krw = ""
+  decimal = parseInt(decimal)
+  if (decimal > 억) {
+    var num = Math.floor(decimal / 억)
+    krw = num.toString() + "억"
+    decimal = decimal % 억
+  }
+  if (decimal > 만) {
+    if (krw != "") {
+      krw = krw + " "
+    }
+    var num = Math.floor(decimal / 만)
+    krw = krw + num.toString() + "만"
+    decimal = decimal % 만
+  }
+  if (krw != "") {
+    krw = krw + " "  
+  }
+  krw = krw + decimal.toString()
+  return krw
 }
 
 module.exports.function = function getBTCRates (coins) {
@@ -52,9 +99,9 @@ module.exports.function = function getBTCRates (coins) {
   }
   var result = {
     price: {
-      last: 1,
-      high: 1,
-      low: 1,
+      last: "1",
+      high: "1",
+      low: "1",
       change: {
         percentage: 0,
         absolute: 0
@@ -74,6 +121,16 @@ module.exports.function = function getBTCRates (coins) {
   }
   var response = http.getUrl(targetMarketBaseUrl + '/' + coins.coin.toLowerCase() + coins.exchange.toLowerCase() + '/summary', { format: 'json' })
   result.price = response.result.price
+  for (var key in result.price) {
+    if (key != 'last' && key != 'high' && key != 'low') {
+      continue
+    }
+    result.price[key] = result.price[key].toString()
+    result.price[key] = scientificToDecimal(result.price[key])
+    if (coins.exchange.toLowerCase() == 'krw') {
+      result.price[key] = decimalToKRW(result.price[key])
+    }
+  }
   result.volume = response.result.volume
   return result
 }
